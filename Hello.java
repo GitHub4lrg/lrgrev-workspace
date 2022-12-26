b451eb64e12e038252f6fd31f65751b5cb91b054
Luis R Guerrero <lrgutube09@gmail.com>
2022-12-26 21:24:53 +0000

    "worked on number app"

---
+++ Hello.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Please input a number: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        if (number > 0) {
+            System.out.println("You input a positive number");
+        }else if(number < 0){
+            System.out.println("You input a negative number");
+        }
+    }
+    /*
+    * Today's topics -
+    * the main method,
+    * the scanner class,
+    * the system.out.println statement,
+    * code compilation
+    * if/else statement
+    */
+}
